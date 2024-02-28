dc0aaa91014f9deb47e97883dc4b162319d5fbcb
s4yoo <77596495+s4yoo@users.noreply.github.com>
2024-02-28 16:29:12 +0900

    Update Minesweeper.c

--- Minesweeper.c
+++ Minesweeper.c
@@ -32,13 +32,11 @@ void initializeBoard() {
 		for (int j = 0; j < SIZE; j++) {
 			if (board[i][j] != -1) {
 				int sum = 0;
-				int count = 0;
 
 				for (int x = i - 1; x <= i + 1; x++) {
 					for (int y = j - 1; y <= j + 1; y++) {
 						if (x >= 0 && x < SIZE && y >= 0 && y < SIZE) {
 							sum += board[x][y];
-							count++;
 						}
 					}
 				}
